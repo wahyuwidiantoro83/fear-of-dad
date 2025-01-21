23824267eb6e6bd131e6962c5db36d2f0b33a8e2
wahyuwidiantoro83 <wahyuwidiantoro83@gmail.com>
2025-01-22 06:50:25 +0700

    creating login page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Navbar from "./store/Navbar";
 import Footer from "./store/Footer";
 
@@ -13,7 +14,23 @@ const Layout = (props) => {
             openModal ? "top-20" : "-top-full"
           } left-0 right-0 bg-white z-[99] transition-all ease-in-out duration-700`}
         >
-          <div className="flex w-full h-full">Halo</div>
+          <div className="flex w-full h-full">
+            <Tabs defaultValue="man" className="w-full">
+              <TabsList className="flex w-full justify-evenly py-0">
+                <TabsTrigger className="w-full rounded-none" value="man">
+                  MAN
+                </TabsTrigger>
+                <TabsTrigger className="w-full rounded-none" value="woman">
+                  WOMAN
+                </TabsTrigger>
+                <TabsTrigger className="w-full rounded-none" value="kids">
+                  KIDS
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent value="man">Make changes to your account here.</TabsContent>
+              <TabsContent value="woman">Change your password here.</TabsContent>
+            </Tabs>
+          </div>
         </div>
         {props.children}
         <Footer />
--- src/pages/auth/LoginView.jsx
+++ src/pages/auth/LoginView.jsx
@@ -1,61 +1,77 @@
+import Logo from "../../assets/logo.png";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { SignIn } from "@clerk/clerk-react";
+import { Checkbox } from "@/components/ui/checkbox";
 
 const LoginView = () => {
   return (
-    <div className="h-dvh w-dvw flex justify-center items-center">
-      <SignIn signUpUrl="/signup" />
-      {/* <Card className="m-auto w-[350px]">
-        <CardHeader>
-          <CardTitle>Create project</CardTitle>
-          <CardDescription>Deploy your new project in one-click.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" placeholder="Name of your project" />
-              </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="framework">Framework</Label>
-                <Select>
-                  <SelectTrigger id="framework">
-                    <SelectValue placeholder="Select" />
-                  </SelectTrigger>
-                  <SelectContent position="popper">
-                    <SelectItem value="next">Next.js</SelectItem>
-                    <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                    <SelectItem value="astro">Astro</SelectItem>
-                    <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                  </SelectContent>
-                </Select>
-              </div>
+    <div className="h-svh w-full font-poppins">
+      <div className="flex w-full items-center border-b border-gray-200 shadow-sm h-20 px-6 md:px-12 lg:px-36">
+        <span className="w-12 h-12">
+          <img className="w-full h-full" src={Logo} alt="" srcSet="" />
+        </span>
+      </div>
+      <div className="flex w-full justify-center px-6 md:px-12 lg:px-36 py-6 md:py-12">
+        <div className="flex border border-gray-200 w-full divide-x gap-2 divide-gray-200">
+          <div className="flex px-4 py-8 w-full flex-col gap-4 md:gap-6 items-start">
+            <h1 className="text-4xl font-semibold">LOGIN</h1>
+            <p>Login with your email address and password.</p>
+            <div className="flex flex-col w-full gap-1">
+              <label className="text-xl font-medium" htmlFor="">
+                EMAIL ADDRESS *
+              </label>
+              <input
+                className="bg-gray-100 border-b border-black focus:bg-white focus:border-b-2 outline-offset-4 outline-gray-400 p-4 placeholder:text-sm"
+                type="text"
+                placeholder="Input your email"
+              />
             </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button variant="outline">Cancel</Button>
-          <Button>Deploy</Button>
-        </CardFooter>
-      </Card> */}
+            <div className="flex flex-col w-full gap-1">
+              <label className="text-xl font-medium" htmlFor="">
+                PASSWORD *
+              </label>
+              <input
+                className="bg-gray-100 border-b border-black focus:bg-white focus:border-b-2 outline-offset-4 outline-gray-400 p-4 placeholder:text-sm"
+                type="password"
+                placeholder="Input your password"
+              />
+              <small className="text-xs text-gray-400">
+                The password must be at least 8 characters long, and consists of letters and
+                numbers. Only the -_.@ symbol can be used.
+              </small>
+            </div>
+            <div className="flex items-center gap-1">
+              <Checkbox className="rounded-none" />
+              <label htmlFor="">Show Password</label>
+            </div>
+            <div className="flex flex-col items-start gap-1">
+              <span className="font-semibold underline cursor-pointer">TERM OF USE</span>
+              <span className="font-semibold underline cursor-pointer">PRIVACY POLICY</span>
+            </div>
+            <div className="flex items-start flex-col gap-1">
+              <Button className="w-80 text-lg font-bold rounded-sm py-6" type="submit">
+                LOGIN
+              </Button>
+              <span className="font-semibold underline cursor-pointer">FORGOT YOUR PASSWORD?</span>
+            </div>
+          </div>
+          <div className="flex px-4 py-8 w-full flex-col gap-4">
+            <h1 className="text-4xl font-semibold">BUAT AKUN</h1>
+            <p>
+              If you create an account, you can get personalized services such as viewing your
+              purchase history and getting discount coupons with your membership. Sign up today,
+              it's free!
+            </p>
+            <Button className="w-80 mt-4 text-lg font-bold rounded-sm py-6" type="submit">
+              BUAT AKUN
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full pb-4 justify-center">
+        <p className=" uppercase text-sm font-semibold text-center">
+          FEAR OF DAD ©️ {new Date().getFullYear()} BY WAHYU WIDIANTORO (FOR LEARNING PURPOSE ONLY)
+        </p>
+      </div>
     </div>
   );
 };
